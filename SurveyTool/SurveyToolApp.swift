df899be54c4afac1d208edc692f38dd513a96c68
Andrii Pashuta <andrii.pashuta@gmail.com>
2025-02-26 22:36:19 +0100

    Initial Commit

---
+++ SurveyTool/SurveyToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyToolApp.swift
+//  SurveyTool
+//
+//  Created by Andrii Pashuta on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SurveyToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
